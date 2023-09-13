b9caf99af2eed837be23623bf9cca2f2c00e378a
Babuthetitan <landiasteve@gmail.com>
2023-09-13 16:44:53 +0100

    SkyHigh workflow 1
    1. Asteroids going up while gems are coming down
    2. Setting up randomization of gems
    3. Set up randomization of asteroids

--- SkyHigh/MainGame.cpp
+++ SkyHigh/MainGame.cpp
@@ -15,14 +15,14 @@ enum GameObjectType
 };
 
 // Number of gems to create
-const int NUM_GEMS = 6;
+const int NUM_GEMS = 3;
 // Gem radius
 const float GEM_RADIUS = 20.0f;
 // Gem speed
 const float GEM_SPEED = 1.0f;
 
 //Number of Asteroids
-const int NUM_ASTEROIDS = 2;
+const int NUM_ASTEROIDS = 3;
 // Asteroid radius
 const float ASTEROID_RADIUS = 30.0f;
 // Asteroid speed
@@ -41,7 +41,7 @@ void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
     Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
     Play::CentreAllSpriteOrigins();
     Play::LoadBackground( "Data\\Backgrounds\\background1.png" );
-    GemCreation();
+    GemCreation(); 
     AsteroidCreation();
 }
 
@@ -50,9 +50,7 @@ bool MainGameUpdate( float elapsedTime )
 {
     GemBehaviour();
     AsteroidBehaviour();
-
     Draw();
-
     return Play::KeyDown( VK_ESCAPE );
 }
 
@@ -65,20 +63,26 @@ int MainGameExit( void )
 
 void Draw()
 {
-    
     Play::DrawBackground();
     
     // Draw the Gems
-    for (int i = 0; i < NUM_GEMS; i++)
+    Play::CollectGameObjectIDsByType(TYPE_GEM);
+    std::vector<int> GemIDs = Play::CollectGameObjectIDsByType(TYPE_GEM);
+    for (int i: GemIDs)
     {
-        Play::DrawObject(Play::GetGameObjectByType(TYPE_GEM));
+        Play::DrawObject(Play::GetGameObject(i));
+        Play::GetGameObject(i);
     }
 
     // Draw the asteroids
-    for (int i = 0; i < NUM_ASTEROIDS; i++)
+    Play::CollectGameObjectIDsByType(TYPE_ASTEROID);
+    std::vector<int> AsteroidIDs = Play::CollectGameObjectIDsByType(TYPE_ASTEROID);
+    for (int i :AsteroidIDs)
     {
-        Play::DrawObject(Play::GetGameObjectByType(TYPE_ASTEROID));
+        Play::DrawObject(Play::GetGameObject(i));
+        Play::GetGameObject(i);
     }
+    
 
     Play::PresentDrawingBuffer();
 
@@ -109,12 +113,13 @@ void AsteroidCreation()
 void GemBehaviour()
 {
     // Gem Behaviour going into update
-    for (int i = 0; i < NUM_GEMS; i++)
+    std::vector<int> GemIDs = Play::CollectGameObjectIDsByType(TYPE_GEM);
+    for (int i : GemIDs)
     {
-        GameObject& obj_gem = Play::GetGameObjectByType(TYPE_GEM);
+        GameObject& obj_gem = Play::GetGameObject(i);
         obj_gem.pos.y += GEM_SPEED;
 
-        //checking if Gem is out of the display area and reset its position
+        //checking if Gems are out of the display area and reset their position
         if (obj_gem.pos.y > DISPLAY_HEIGHT + GEM_RADIUS)
         {
             obj_gem.pos.y = -GEM_RADIUS;
@@ -126,31 +131,20 @@ void GemBehaviour()
 void AsteroidBehaviour()
 {
     //Asteroid Behaviour going into update
-    for (int i = 0; i < NUM_ASTEROIDS; i++)
+    std::vector<int> AsteroidIDs = Play::CollectGameObjectIDsByType(TYPE_ASTEROID);
+    for (int i : AsteroidIDs)
     {
-        GameObject& obj_asteroid = Play::GetGameObjectByType(TYPE_ASTEROID);
+        GameObject& obj_asteroid = Play::GetGameObject(i);
 
-        // Move the asteroid in its current direction
-        obj_asteroid.pos.x += ASTEROID_SPEED * cos(obj_asteroid.rotation);
-        obj_asteroid.pos.y += ASTEROID_SPEED * sin(obj_asteroid.rotation);
+        // Moving the asteroids upward
+        obj_asteroid.pos.y -= ASTEROID_SPEED;
 
-        // Wrapping the asteroid to the opposite display area if out of bounds
-        if (obj_asteroid.pos.x < -ASTEROID_RADIUS)
-        {
-            obj_asteroid.pos.x = DISPLAY_WIDTH + ASTEROID_RADIUS;
-        }
-        else if (obj_asteroid.pos.x > DISPLAY_WIDTH + ASTEROID_RADIUS)
-        {
-            obj_asteroid.pos.x = -ASTEROID_RADIUS;
-        }
-
+        // Wrapping the asteroid to the opposite display area at random positions if out of bounds
         if (obj_asteroid.pos.y < -ASTEROID_RADIUS)
         {
+            obj_asteroid.pos.x = static_cast<float>(rand() % DISPLAY_WIDTH);
             obj_asteroid.pos.y = DISPLAY_HEIGHT + ASTEROID_RADIUS;
-        }
-        else if (obj_asteroid.pos.y > DISPLAY_HEIGHT + ASTEROID_RADIUS)
-        {
-            obj_asteroid.pos.y = -ASTEROID_RADIUS;
         }
     }
 }
+
